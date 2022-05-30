965f05063feb275f5ce5d30ac1a3298eedd30252
Jacek Motyka <jmotyka9@gmail.com>
2022-05-30 22:29:49 +0200

    query for finding all id's of Tasks assigned to a particular users was added

--- src/main/java/com/lft/taskservice/tasks/adapters/persistance/JpaAssignmentRepository.java
+++ src/main/java/com/lft/taskservice/tasks/adapters/persistance/JpaAssignmentRepository.java
@@ -1,7 +1,13 @@
 package com.lft.taskservice.tasks.adapters.persistance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface JpaAssignmentRepository extends JpaRepository<AssignmentEntity, Long> {
+
+    @Query("select a.taskId from AssignmentEntity a where a.userId=:userId")
+    List<Long> findAllTasksIdByUserId(Long userId);
 
 }
