e4ac04cd3f91089d8c0006416c51194372d7c479
Jacek Motyka <jmotyka9@gmail.com>
2022-05-30 18:22:06 +0200

    RestAssignmentMapper added

---
+++ src/main/java/com/lft/taskservice/tasks/adapters/rest/RestAssignmentMapper.java
@@ -0,0 +1,21 @@
+package com.lft.taskservice.tasks.adapters.rest;
+
+import com.lft.taskservice.tasks.domain.Assignment;
+import com.lft.taskservice.tasks.domain.Task;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestAssignmentMapper {
+
+    private final ModelMapper mapper = new ModelMapper();
+
+    Assignment toDomain(AssignmentDto assignmentDto) {
+        return mapper.map(assignmentDto, Assignment.class);
+    }
+
+    AssignmentDto toDto(Assignment assignment) {
+        return mapper.map(assignment, AssignmentDto.class);
+    }
+
+}
