c1473cdcf2b8424274fd4976b811842c3aaac14f
Jacek Motyka <jmotyka9@gmail.com>
2022-06-01 20:50:11 +0200

    added iterable mapping in all mappers

--- src/main/java/com/lft/taskservice/tasks/adapters/rest/RestAssignmentMapper.java
+++ src/main/java/com/lft/taskservice/tasks/adapters/rest/RestAssignmentMapper.java
@@ -1,8 +1,12 @@
 package com.lft.taskservice.tasks.adapters.rest;
 
+import com.lft.taskservice.tasks.adapters.logging.TaskLogging;
 import com.lft.taskservice.tasks.domain.Assignment;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class RestAssignmentMapper {
@@ -16,6 +20,14 @@ public class RestAssignmentMapper {
 
     AssignmentDto toDto(Assignment assignment) {
         return mapper.map(assignment, AssignmentDto.class);
+    }
+
+    @TaskLogging
+    <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+        return source
+                .stream()
+                .map(element -> mapper.map(element, targetClass))
+                .collect(Collectors.toList());
     }
 
 }
