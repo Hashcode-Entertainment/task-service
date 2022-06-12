9dc8db5c421a93b78b1619fbd1028fe420166739
Jacek Motyka <jmotyka9@gmail.com>
2022-06-12 16:37:40 +0200

    test save() method

--- src/test/java/com/lft/taskservice/tasks/adapters/persistance/JpaAssignmentRepositoryTest.java
+++ src/test/java/com/lft/taskservice/tasks/adapters/persistance/JpaAssignmentRepositoryTest.java
@@ -21,12 +21,24 @@ class JpaAssignmentRepositoryTest {
     @Autowired
     JpaTaskRepository taskRepository;
 
-
-
     @DisplayName("Empty database is initialized. Should return no records")
     @Test
     void shouldReturnNoRecords() {
         assertEquals(0, assignmentRepository.findAllTasksAssignedToUser(1L).size());
+    }
+
+    @Test
+    @DirtiesContext
+    void shouldSaveAssignmentToTheDatabase() {
+        populateTasks();
+        assignmentRepository.save(
+                AssignmentEntity.builder()
+                        .task(taskRepository.getReferenceById(1L))
+                        .userId(666L)
+                        .deadline(LocalDate.now())
+                        .assignedOn(LocalDate.now())
+                        .build());
+        assertEquals(1, assignmentRepository.findAll().size());
     }
 
     void populateTasks(){
